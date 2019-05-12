11e56cfff114756a704b1ac7c966cb67e32dbfdb
JustinS1998 <32559440+JustinS1998@users.noreply.github.com>
2019-05-11 23:31:07 -0700

    Asteroid spawning
    Spawn asteriods with y key with random starting force

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class AsteroidController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 oldVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        oldVelocity = rb.velocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.tag == "Boundary")
+        {
+            rb.velocity = oldVelocity;
+        }
+        if (collision.collider.tag == "Bullet")
+        {
+            //break();
+            Destroy(collision.collider.gameObject);
+        }
+    }
+    private void LateUpdate()
+    {
+        oldVelocity = rb.velocity;
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int level;
+    public int numAsteroids;
+    public GameObject asteroid;
+    public float startingForce;
+
+    private void Start()
+    {
+        level = 0;
+        numAsteroids = 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("y"))
+        {
+            for (int i = 0; i < numAsteroids; i++)
+            {
+                GameObject newAsteroid = Instantiate(asteroid);
+                Rigidbody2D newrb = newAsteroid.GetComponent<Rigidbody2D>();
+                newrb.MovePosition(new Vector2(3, 3));
+                newrb.MoveRotation(Random.value*360);
+                newrb.AddForce(newAsteroid.transform.up * startingForce);
+            }
+        }
+    }
+}
